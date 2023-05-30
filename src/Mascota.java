3f3362e2469da540bf5ae61662d9fb4f717554c2
Uri Mtz <39018208+UriMtzF@users.noreply.github.com>
2023-05-29 22:20:42 -0600

    Creados atributos clase padre Mascota

--- src/Mascota.java
+++ src/Mascota.java
@@ -1,2 +1,14 @@
 public class Mascota {
+    protected int clave;
+    protected String nombreDueno;
+    protected String nombreMascota;
+    protected int edadMascota;
+    protected String direccion;
+    protected String telefono;
+    protected int prioridad;
+    protected int precioConsulta;
+    protected String nombreArchivo;
+
+    public Mascota() {
+    }
 }
